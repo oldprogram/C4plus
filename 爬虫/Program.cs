3b9ae4e1ce82ac5aa1b4d2bd0c3e2728e77487b3
litao <953521476@qq.com>
2015-02-23 09:30:56 +0800

    分析百度搜索结果的特点进行提取
    100%提取出链接，但是有时候一个也提取不了，可能是网络问题

--- 爬虫/Program.cs
+++ 爬虫/Program.cs
@@ -18,41 +18,63 @@ namespace ConsoleApplication1
             Stream strm = client.OpenRead("http://www.baidu.com/s?word=hdu%201202&pn=0");
             StreamReader sr = new StreamReader(strm);
             string line;
-            StreamWriter sw = new StreamWriter("D:\\1.txt");
+            StreamWriter sw = new StreamWriter("D:\\过程.txt");
+            StreamWriter sw2 = new StreamWriter("D:\\结果.txt");
             int num = 1;
-
+            int find1=-1;
             while ((line = sr.ReadLine()) != null)
             {
-                for (int i = 0; i < line.Length - 7; i++)//找line中的http开头的链接
-                {
-                    if (line[i] == 'h')
-                    {
-                        if (line[i + 1] == 't' && line[i + 2] == 't' && line[i + 3] == 'p' && line[i + 4] == ':')
-                        {
-                            int last = i + 7;
-                            while (last < line.Length && line[last++] != '"') ;
-                            last--;
-                            if (line[last] == '"')
-                            {
-                                string temp = line.Substring(i, last - i);
-                                int find_blog = temp.IndexOf("blog");
-                                int find_baidu = temp.IndexOf("baidu");
-                                if (find_blog != -1 && find_baidu==-1)
-                                {
-                                    temp = temp.Replace("\\", "");
-                                    Console.WriteLine("["+num.ToString()+"]"+temp);
-                                    sw.WriteLine("[" + num.ToString() + "]" + temp);
-                                    client.DownloadFile(temp, "D:\\download"+num.ToString() + ".html");
-                                    num++;
-                                }
-                            }
-                        }
-                    }
-                }
-               // sw.WriteLine("\n--------------------------------------------------------------\n");
+                sw.WriteLine("1: " + line);
+                find1 = line.IndexOf("content_left");
+                if(find1!=-1)break;
             }
+            if(find1!=-1)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    int find2 = -1;
+                    do{
+                        sw.WriteLine("2: " + line);
+                        find2 = line.Substring(find1).IndexOf("result c-container ");
+                        if (find2 != -1) break;
+                        find1 = 0;
+                    } while ((line = sr.ReadLine()) != null);
+                    if (find2 == -1) break;
+
+                    int find3 = -1;
+                    do{
+                        sw.WriteLine("3: " + line);
+                        find3 = line.Substring(find1+find2).IndexOf("href");
+                        if (find3 != -1) break;
+                        find1 = find2 = 0;
+                    } while ((line = sr.ReadLine()) != null);
+                    if (find3 == -1) break;
+
+                    int find4 = -1;
+                    string temp = "";
+                    do{
+                        sw.WriteLine("4: " + line);
+                        find4 = line.Substring(find1 + find2 + find3 + 8).IndexOf("\"");
+                        if (find4 != -1) break;
+                        temp += line.Substring(find1 + find2 + find3 + 8);
+                        find1 = find2 = find3 = 0;
+                    } while ((line = sr.ReadLine()) != null);
+                    if (find4 == -1) break;
+
+                    temp += line.Substring(find1+find2+find3 + 8, find4);
+                    
+                    Console.WriteLine("[" + num.ToString() + "]" + temp);
+                    sw2.WriteLine("[" + num.ToString() + "]" + temp);
+                    client.DownloadFile(temp, "D:\\download" + num.ToString() + ".html");
+                    num++;
+                    //find1 = find1 + find2 + find3 + find4;
+                }
+            }
+            
             sw.Close();
+            sw2.Close();
             strm.Close();
+            Console.Read();
         }
     }
 }
